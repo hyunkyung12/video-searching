f225502c16ffd03654e01c2f7fd5a7a835d22c1b
hyunkyung12 <khc0275@naver.com>
2018-06-03 18:42:39 +0900

    hyeonjin2

---
+++ src/find_exec_target.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+import sys
+import os
+
+file_name = sys.argv[1]
+funct = sys.argv[2]
+input_w = sys.argv[3]
+
+path = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+demo = pd.read_csv( path + "/data/" + file_name +".csv" )
+demo = demo.dropna()
+
+def exec_find(word,table):
+    result = table[['subtitle']].apply(lambda x: x.values[0].find(word),axis = 1 )
+    result = table[['url','start','end']][result != -1]
+    return result
+
+def target_find(table, func,target):
+    result = func(target,table)   
+    result.to_csv(path + "/data" +'/check_list.csv')
+    return result
+
+if __name__ == "__main__":
+
+    print("...strat")
+    target_find(demo,exec_find,input_w)
+    print("...end")
+
+
+    
