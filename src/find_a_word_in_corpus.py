1bfa240244897d2ba1665e5eef39ab2662287f39
HyeonJin1030 <sherlock961030@gmail.com>
2018-06-03 18:07:00 +0900

    find exec target

--- src/find_a_word_in_corpus.py
+++ src/find_a_word_in_corpus.py
@@ -3,8 +3,6 @@ import numpy as np
 import sys
 import os
 
-
-
 file_name = sys.argv[1]
 funct = sys.argv[2]
 input_w = sys.argv[3]
@@ -13,33 +11,21 @@ path = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 demo = pd.read_csv( path + "/data/" + file_name +".csv" )
 demo = demo.dropna()
 
-def a_word(word,table):
-
-    corpus = [[word for word in line.split()] for line in table["subtitle"]]
-
-    index = np.zeros(0)
-
-    for i in range(len(table)):
-        tmp = corpus[i]
-        check = sum(np.array(tmp) == word)
-        if check > 0:
-            index = np.append(index,1)
-        else:
-            index =  np.append(index,0)
-
-    result = table[index == 1][['url','start','end']]
-    result = result.reset_index()[['url','start','end']]
+def exec_find(word,table):
+    result = table[['subtitle']].apply(lambda x: x.values[0].find(word),axis = 1 )
+    result = table[['url','start','end']][result != -1]
     return result
 
-def find(table, func,find):
-    result = func(find,table)   
-    result.to_csv(path + '/data/check_list.csv')
+def target_find(table, func,target):
+    result = func(target,table)   
+    result.to_csv(path + "/data/" +'/check_list.csv')
     return result
 
 if __name__ == "__main__":
 
-    if funct == 0:
-        find(demo,a_word,input_w)
+    print("...strat")
+    target_find(demo,exec_find,input_w)
+    print("...end")
 
 
     
