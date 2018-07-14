e538705451ff11533185f2ff5dc7dfbc346ec2ec
HyeonJin1030 <sherlock961030@gmail.com>
2018-07-14 10:21:27 +0900

    db

--- src/preprocessing/tokenizing.py
+++ src/preprocessing/tokenizing.py
@@ -12,14 +12,14 @@ from konlpy.utils import pprint
 
 import sqlite3
 
-con = sqlite3.connect('data/youtubing.db')
+con = sqlite3.connect('data/youtubing_dev.db')
 
 #파일 불러오기
 sql_stc = "SELECT \
                 A.sentence_id as sentence_id, \
-                A.start_time as start, \
-                A.end_time as end, \
-                A.sentence as subtitle, \
+                A.start_time as start_time, \
+                A.end_time as end_time, \
+                A.sentence as sentence, \
                 A.text_token as text_token, \
                 A.embedding_vector as embedding_vector, \
                 A.subtitle_id as subtitle_id, \
@@ -43,8 +43,8 @@ print(len(kor_demo))
 if len(kor_demo) != 0:
     
     kkma = Kkma()
-    kk_tag = kor_demo.apply(lambda row:kkma.pos(row['subtitle']), axis=1)
-    kor_demo['morphs'] = kor_demo.apply(lambda row:kkma.morphs(row['subtitle']), axis=1)
+    kk_tag = kor_demo.apply(lambda row:kkma.pos(row['sentence']), axis=1)
+    kor_demo['morphs'] = kor_demo.apply(lambda row:kkma.morphs(row['sentence']), axis=1)
     print("complete.\n")
 #korea 형태소 분석 (명사, 동사)
     print("kkma refined ...")
@@ -54,7 +54,7 @@ if len(kor_demo) != 0:
     for j in range(len(kor_demo)):
         indice = np.array( [ 1 if v in lst else 0 for (k,v) in kk_tag.values[j]] )
         temp = list(np.array(kor_demo['morphs'].values[j])[np.array(indice,dtype = bool)])
-        result.append(temp)
+        result.append(str(temp))
         
     kor_demo['text_token'] = result
 print("complete.")
@@ -66,8 +66,8 @@ print("nltk ...")
 print(len(eng_demo))
 if len(eng_demo != 0):
     
-    eng_demo['morphs'] = eng_demo.apply(lambda row: nltk.word_tokenize(row['subtitle']), axis=1)
-    nltk_tag = eng_demo.apply(lambda row:nltk.pos_tag(nltk.word_tokenize(row['subtitle'])), axis=1)
+    eng_demo['morphs'] = eng_demo.apply(lambda row: nltk.word_tokenize(row['sentence']), axis=1)
+    nltk_tag = eng_demo.apply(lambda row:nltk.pos_tag(nltk.word_tokenize(row['sentence'])), axis=1)
     print("complete.\n")
 
 #english 형태소 분석 (명사, 동사)
@@ -78,7 +78,7 @@ if len(eng_demo != 0):
     for j in range(len(eng_demo)):
         nltk_indice = np.array( [ 1 if v in lst else 0 for (k,v) in nltk_tag.values[j]] )
         nltk_temp = list(np.array(eng_demo['morphs'].values[j])[np.array(nltk_indice,dtype = bool)])
-        result.append(nltk_temp)
+        result.append(str(nltk_temp))
         
     eng_demo['text_token'] = result
 print("complete.\n")
@@ -93,12 +93,13 @@ result = pd.concat(frames)
 result = result.reset_index()
 
 #저장
-result.to_csv("morphs.csv")
+#result.to_csv("morphs.csv")
 
 result.drop(['morphs'], axis=1)
 
-#del_sql = "DELETE FROM sentence_meta WHERE text_token IS null"
-#cur = con.cursor()
-#cur.execute(del_sql)
+del_sql = "DELETE FROM sentence_meta WHERE text_token IS null"
+cur = con.cursor()
+cur.execute(del_sql)
 
-#result.to_sql('sentence_meta', con, if_exists='append', index=False)
+save_result = result[['sentence_id', 'start_time', 'end_time', 'sentence', 'text_token', 'subtitle_id']]
+save_result.to_sql('sentence_meta', con, if_exists='append', index=False)
